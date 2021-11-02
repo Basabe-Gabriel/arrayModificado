15e587ea78fc6fb2a235d45d62483c78961fffa0
Basabe-Gabriel <92063962+Basabe-Gabriel@users.noreply.github.com>
2021-11-01 21:09:51 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let nombre: string[] = new Array(4);
+let ingreseNombre: string;
+
+for (let i = 0; i <= 3; i++) {
+  //ingreseNombre = prompt("ingrese nombre");
+  nombre[i] = ingreseNombre;
+}
+for (let i = 0; i <= 3; i++) {
+  console.log(nombre[i]);
+}
